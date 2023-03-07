498ada59eb5d4fcaecdb79f359872182cf84ea39
tiero <3596602+tiero@users.noreply.github.com>
2023-03-07 18:44:40 +0100

    add scure/btc-signer

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as btc from 'micro-btc-signer';
+import { hex } from '@scure/base';
 
 export default function App() {
+  const PubKey = hex.decode('0101010101010101010101010101010101010101010101010101010101010101');
+  console.log(btc.p2tr(PubKey).address);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
